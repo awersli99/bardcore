c5f000b717bae3c990a003d33832b6b72cd80ceb
Adam <30961223+awersli99@users.noreply.github.com>
2023-02-04 17:01:00 +0000

    Initial commit

---
+++ src/main/java/turniplabs/examplemod/ExampleMod.java
@@ -0,0 +1,16 @@
+package turniplabs.examplemod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class ExampleMod implements ModInitializer {
+    public static final String MOD_ID = "examplemod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("ExampleMod initialized.");
+    }
+}
